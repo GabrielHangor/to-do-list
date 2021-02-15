982c6a110ca409602e5a2050edfa23f5c4ab04f1
HangorG <gaba93@yandex.ru>
2021-02-15 21:32:55 +0300

    animated hamburger menu icon

--- src/DOM.js
+++ src/DOM.js
@@ -2,14 +2,15 @@
 const sideBarToggle = (function () {
   const sidebar = document.querySelector(".sidebar");
   const mainContent = document.querySelector(".main-content");
-  const hamburgerIcon = document.querySelector("#hamburgerIcon");
+  const menuBars = document.querySelector(".menu-bars");
 
   function toggleSidebar() {
     sidebar.classList.toggle("sidebar-toggle");
     mainContent.classList.toggle("main-content-toggle");
+    menuBars.classList.toggle("change");
   }
 
-  hamburgerIcon.addEventListener("click", toggleSidebar);
+  menuBars.addEventListener("click", toggleSidebar);
 })();
 
 export { sideBarToggle };
